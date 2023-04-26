8e4463587d974d50ba7abf55cd6cef94ec62ba2f
cvicens <carlos.vicens.alonso@gmail.com>
2023-04-26 16:16:54 +0200

    updated

--- fruit-gateway/image-run.sh
+++ fruit-gateway/image-run.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)
 
 . ./image-env.sh
 
-podman run -it --rm -p 8080:8080 -e OTL_ENDPOINT=http://192.168.50.17:4317 -e FRUIT_SERVICE_URL=http://192.168.50.17:8081 -e JAVA_OPTS="-Dspring.profiles.active=openshift" $REGISTRY/$REGISTRY_USER_ID/${PROJECT_ID}-${ARTIFACT_ID}:${GIT_HASH}
+podman run -it --rm -p 8080:8080 -e OTL_ENDPOINT=http://192.168.50.105:4317 -e FRUIT_SERVICE_URL=http://192.168.50.105:8081 --name gateway $REGISTRY/$REGISTRY_USER_ID/${PROJECT_ID}-${ARTIFACT_ID}:${GIT_HASH}
 
 cd ${CURRENT_DIR}
