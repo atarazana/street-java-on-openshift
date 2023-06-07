64ffa05512f3cc43e4a5582e3604a2a6a377f597
cvicens <carlos.vicens.alonso@gmail.com>
2023-06-07 17:12:46 +0200

    change to use local image and additional env vars

--- fruit-gateway/image-run.sh
+++ fruit-gateway/image-run.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)
 
 . ./image-env.sh
 
-podman run -it --rm -p 8080:8080 -e OTL_ENDPOINT=http://192.168.50.105:4317 -e FRUIT_SERVICE_URL=http://192.168.50.105:8081 --name gateway $REGISTRY/$REGISTRY_USER_ID/${PROJECT_ID}-${ARTIFACT_ID}:${GIT_HASH}
+podman run -it --rm -p 8080:8080 -e FRUIT_GATEWAY_APP_NAME=fruit-gateway-user1 -e OTL_ENDPOINT=http://192.168.50.104:4317 -e FRUIT_SERVICE_URL=http://192.168.50.104:8081 --name gateway localhost/street-java-fruit-gateway:${ARTIFACT_VERSION}
 
 cd ${CURRENT_DIR}
